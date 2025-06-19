4f7b0c0e3f611026d632b644b9e0d5584fb63d09
kathirselvan761 <kathirsukitha@gmail.com>
2025-06-19 19:52:18 +0530

    june 19 updates kathir

--- frontend/src/components/ProductCarousel.js
+++ frontend/src/components/ProductCarousel.js
@@ -22,16 +22,16 @@ const ProductCarousel = () => {
   ) : error ? (
     <Message variant='danger'>{error}</Message>
   ) : (
-    <Carousel pause='hover' className='bg-dark'>
+    <Carousel pause='hover' className='product-carousel'>
       {products.map((product) => (
         <Carousel.Item key={product._id}>
-          <Link to={`/product/${product._id}`}>
-            <Image src={product.image} alt={product.name} fluid />
-            <Carousel.Caption className='carousel-caption'>
-              <h2>
-                {product.name} (${product.price})
-              </h2>
-            </Carousel.Caption>
+          <Link to={`/product/${product._id}`} className='carousel-link'>
+            <div className='carousel-card'>
+              <Image src={product.image} alt={product.name} className='carousel-img' />
+              <Carousel.Caption className='carousel-caption'>
+                <h5>{product.name} (${product.price})</h5>
+              </Carousel.Caption>
+            </div>
           </Link>
         </Carousel.Item>
       ))}
